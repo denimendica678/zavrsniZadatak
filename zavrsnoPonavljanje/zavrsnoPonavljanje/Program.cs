8220e9d30e6f765792e916af995dbdcaf0988caa
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-05-31 13:07:06 +0200

    Add files via upload

---
+++ zavrsnoPonavljanje/zavrsnoPonavljanje/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoPonavljanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nizZnakova, sVelika, sMala, sPrvaTri, sZadnjihPet, s8_11;
+
+            Console.WriteLine("Unesite niz znakova koji nije manji od 11:");
+            nizZnakova = Console.ReadLine();
+
+            sVelika = nizZnakova.ToUpper();
+            Console.WriteLine(sVelika);
+
+            sMala = nizZnakova.ToLower();
+            Console.WriteLine(sMala);
+
+            
+            Console.WriteLine(nizZnakova.Substring(0, 3));
+
+            Console.WriteLine(nizZnakova.Substring(nizZnakova.Length-5));
+
+            Console.WriteLine(nizZnakova.Substring(8, 11));
+
+        }
+    }
+}
